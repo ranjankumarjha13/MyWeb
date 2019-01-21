5059400806129b45cce117d6f2db512400499192
Ranjan <ranjan.smth4@gmail.com>
2019-01-21 15:52:52 +0530

    modification realted to scroll and aligmnet in welcome and view page

--- web/res/scripts/nav.js
+++ web/res/scripts/nav.js
@@ -6,6 +6,14 @@ var CLNav=new function()
     this.getSubmenuitems=function()
     {
       document.getElementById("view").style.display="";;
+        document.getElementById("rightangle").style.display="none";
+        document.getElementById("downangle").style.display="";
+    }
+    this.hidesubmenyitem=function()
+    {
+        document.getElementById("view").style.display="none";;
+        document.getElementById("downangle").style.display="none";
+        document.getElementById("rightangle").style.display="";
     }
     this.getdata=function(ioption)
     {
